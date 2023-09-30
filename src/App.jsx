918da7f6275618a3778e1e860623da805c39b4bb
Leo Cao <leocy29@gmail.com>
2023-09-29 22:53:02 -0700

    use react query apply read and delete cabin feature also using third libary react-hot-toast apply notify message feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { QueryClient } from "@tanstack/query-core"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
 import GlobalStyles from './styles/GlobalStyles'
 import Dashboard from "./pages/Dashboard"
 import Bookings from "./pages/Bookings"
@@ -9,27 +13,60 @@ import Account from "./pages/Account"
 import Login from "./pages/Login"
 import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./ui/AppLayout"
+import { Toaster } from "react-hot-toast"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 function App() {
   return (
-    <>
-    <GlobalStyles />
-    <BrowserRouter>
-      <Routes>
-      <Route element={<AppLayout />}>
-        <Route index element={<Navigate replace to="dashboard"/>}/>
-        <Route path="dashboard" element={<Dashboard />}/>
-        <Route path="bookings" element={<Bookings />}/>
-        <Route path="cabins" element={<Cabins />}/>
-        <Route path="users" element={<Users />}/>
-        <Route path="settings" element={<Settings />}/>
-        <Route path="account" element={<Account />}/>
-       </Route>
-       
-        <Route path="login" element={<Login />}/>
-        <Route path="*" element={<PageNotFound />}/>
-      </Routes>
-    </BrowserRouter>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: '8px' }}
+        toastOptions={{
+          success:
+          {
+            duration: 1000,
+          },
+          error:
+          {
+            duration: 3000,
+          },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)"
+          }
+        }} />
+    </QueryClientProvider>
+
   )
 }
 
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
-
+import { formatCurrency } from "../../utils/helpers"
+import { deleteCabin } from "../../services/apiCabins";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
@@ -38,3 +41,30 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image, id } = cabin;
+  const queryClient = useQueryClient();
+  const { isLoading, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      toast.success("Cabin successfully deleted!")
+      queryClient.invalidateQueries({
+        queryKey: ['cabins']
+      })
+    },
+    onError: err => toast.error(err.message)
+  })
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button onClick={() => mutate(id)} disabled={isLoading}>Delete</button>
+    </TableRow>
+  )
+}
+
+export default CabinRow
+
